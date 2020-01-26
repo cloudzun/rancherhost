e2735eec6d203a45b2edc78363687c048506d72e
CloudZun <info@cloudzun.com>
2020-01-26 20:10:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-az group create --name Ladder --location southeastasia
-az vm create --resource-group Ladder --name myLadder01 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B1s --custom-data SS.txt  
-az vm open-port --resource-group Ladder --name myLadder01 --port 443 --priority 1001
+az group create --name rancherlab --location southeastasia
+az vm create --resource-group rancherlab --name rancherhost --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data SS.txt  
+az vm open-port --resource-group rancherlab --name rancherhost --port 443 --priority 1001
